2ffeba0efc8d4ecdf92bf9a9b3ef30dfa234c612
Ingrid Carlson <ingridclaire@gmail.com>
2020-05-01 17:33:02 -0700

    working on controller/model/seed updates

--- database/seed.js
+++ database/seed.js
@@ -1,12 +1,63 @@
-const site = require('./models/Site.js');
-const price = require('./models/Price.js');
-const hotel = require('./models/Hotel.js');
+const faker = require('faker');
+const Hotel = require('./Model.js');
 
-
+const sites = [];
 let x = 0;
-let newId = 1;
-while (x < 100) {
-  hotel.add(newId);
-  newId += 1;
+while (x < 20) {
+  sites.push(faker.internet.domainName());
   x += 1;
+}
+const chooseRandomSites = () => {
+  const newSites = [];
+  const numberOfSites = Math.ceil(Math.random() * 6);
+  let y = 0;
+  let index = Math.floor(Math.random() * 15);
+  while (y < numberOfSites) {
+    newSites.push(sites[index]);
+    index += 1;
+    y += 1;
+  }
+  return newSites;
+};
+
+let idIndex = 1;
+
+while (idIndex < 101) {
+  const fall = (130 + Math.random() * 100).toFixed(2);
+  const winter = (70 + Math.random() * 100).toFixed(2);
+  const spring = (200 + Math.random() * 100).toFixed(2);
+  const summer = (300 + Math.random() * 100).toFixed(2);
+
+  const newHotel = new Hotel({
+    id: idIndex,
+    name: faker.company.companyName(),
+    fallPrice: {
+      weekday: fall,
+      weekend: fall * 2,
+    },
+    winterPrice: {
+      weekday: winter,
+      weekend: winter * 2,
+    },
+    springPrice: {
+      weekday: spring,
+      weekend: spring * 2,
+    },
+    summerPrice: {
+      weekday: summer,
+      weekend: summer * 2,
+    },
+    otherSites: chooseRandomSites(),
+  });
+  // console.log(newHotel);
+  newHotel.save((err) => {
+    console.log('do we see this');
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('new record saved!');
+    }
+  });
+
+  idIndex += 1;
 }
