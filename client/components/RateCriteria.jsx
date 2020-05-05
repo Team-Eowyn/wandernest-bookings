faa2ec0fae0ad0faf3040ca03e1abd50906b02e4
Ingrid Carlson <ingridclaire@gmail.com>
2020-05-05 15:00:56 -0700

    added buttons, made ViewDeals child component of RateCriteria to manage state better

--- client/components/RateCriteria.jsx
+++ client/components/RateCriteria.jsx
@@ -1,37 +1,104 @@
 import React from 'react';
 import styled from 'styled-components';
+import ViewDeals from './ViewDeals.jsx';
+// import ChooseDates from './ChooseDates.jsx';
 
 const ChooseDates = styled.div`
   display: grid;
-  grid-area: 1 / 1 / span 1 / span 2;
+  grid-area: 1 / 1 / span 1 / span 1;
   height: 49px;
   grid-template: 1fr / 1fr 1fr;
 `;
 
-const GuestsWrapper = styled.div`
-  display: grid;
-  grid-area: 2 / 1 / span 1 / span 2;
-  height: 47px;
+const Checkin = styled.div`
+display: grid;
+grid-area: 1 / 1 / span 1 / span 1
+text-align: left;
+border-style: solid;
+border-color: lightgray;
+border-width: 1px;
+border-radius: 5px;
+margin: 5px 5px 5px 10px;
 `;
 
+const Checkout = styled(Checkin)`
+grid-area: 1 / 2 / span 1 / span 1;
+margin: 5px 10px 5px 5px;
+`;
+
+const GuestsWrapper = styled.div`
+  display: grid;
+  grid-area: 2 / 1 / span 1 / span 1;
+  height: 47px;
+  grid-template: 1fr / 1fr
+`;
+
+const Guests = styled(Checkin)`
+  grid-area: 1 / 1 / span 1 / span 1;
+  margin: 5px 10px;
+`;
+
+const DealsWrapper = styled.div`
+  display: grid:
+  grid-area: 3 / 1 / span 1 / span 1;
+  grid-template: 25px 44px 22px 50px 61px / 1fr
+  margin: 20px 10px;
+`;
 
 class RateCriteria extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      checkin: 'Mon',
-      checkout: 'Wed',
+      checkinDay: 'Mon',
+      checkinDate: '6/10/20',
+      checkoutDay: 'Wed',
+      checkoutDate: '6/12/20',
       rooms: 1,
       adults: 2,
       children: 0,
+      mainPrice: '$350',
     };
   }
 
   render() {
     return (
       <div>
-        <ChooseDates>Choose dates</ChooseDates>
-        <GuestsWrapper>Select Guests</GuestsWrapper>
+        <ChooseDates>
+          <Checkin>
+            <button className="rateCriteria" type="button">
+              <p className="rateCriteria">Check Out</p>
+              <p className="rateCriteria">
+                <strong>{this.state.checkinDay}, {this.state.checkinDate}
+                </strong>
+              </p>
+            </button>
+          </Checkin>
+          <Checkout>
+            <button className="rateCriteria" type="button">
+              <p className="rateCriteria">Check Out</p>
+              <p className="rateCriteria">
+                <strong>
+                  {this.state.checkoutDay}, {this.state.checkoutDate}
+                </strong>
+              </p>
+            </button>
+          </Checkout>
+        </ChooseDates>
+        <GuestsWrapper>
+          <Guests>
+            <button className="rateCriteria" type="button">
+              <p className="rateCriteria">Guests</p>
+              <p className="rateCriteria">
+                <strong>
+                  {this.state.rooms} rooms, {this.state.adults} adults, {this.state.children} children
+                </strong>
+              </p>
+            </button>
+          </Guests>
+        </GuestsWrapper>
+        <DealsWrapper>
+          <ViewDeals mainPrice={this.state.mainPrice}/>
+        </DealsWrapper>
       </div>
     );
   }
--- client/components/ViewDeals.jsx
+++ client/components/ViewDeals.jsx
@@ -4,60 +4,90 @@ import styled from 'styled-components';
 const LowestPrice = styled.p`
   display: grid;
   grid-area: 1 / 1 / span 1 / span 1;
+  height: 25px;
+  font-size: 18px;
   color: gray;
   text-align: center;
-  height: 25px;
+  margin: 10px;
+  padding-top: 5px;
 `;
 
-const MainOfferSite = styled.div`
+const MainOffer = styled.div`
   display: grid;
   grid-area: 2 / 1 / span 1 / span 1;
+  height: 44px;
   grid-template: 1fr / 3fr 1fr;
   color: black;
   font-size: 20px;
-  height: 44px;
+  margin: 10px;
+`;
+const MainOfferSite = styled.p`
+  display: grid;
+  grid-area: 1 / 1 / span 1 / span 1;
+`;
+
+const MainOfferPrice = styled.p`
+  display: grid;
+  grid-area: 1 / 2 / span 1 / span 1;
 `;
 
 const Cancellation = styled.div`
   display: grid;
   grid-area: 3 / 1 / span 1 / span 1;
+  height: 22px;
   font-size: small;
   color: black;
-  height: 22px;
+  margin: 10px;
 `;
 
 const ViewDealWrapper = styled.div`
   display: grid;
   grid-area: 4 / 1 / span 1 / span 1;
   height: 50px;
+  grid-template: 1fr / 1fr
 `;
 
 const ViewDealButton = styled.button`
+  grid-area: 1 / 1 / span 1 / span 1;
   background-color: #ffcc00;
   color: black;
   text-align: center;
-  padding: 15px 5px 15px 5px;
-  border-radius: 15px;
+  padding: 12px 5px 12px 5px;
+  border-radius: 13px;
   margin: 5px 10px 5px 10px;
   font-size: 16px;
-  font-weight: bold;
 `;
 
 const OtherOffers = styled.div`
   display: grid;
   grid-area: 5 / 1 / span 1 / span 1;
+  height: 61px;
+  margin: 10px;
   color: gray;
   font-size: 10px;
   grid-template: 1fr 1fr / 1fr 1fr;
-  height: 61px;
  `;
+
+const Offer1 = styled.p`
+  display: grid;
+  grid-area: 1 / 1 / span 1 / span 1;
+  margin: 10px;
+`;
+const Offer2 = styled(Offer1)`
+  grid-area: 1 / 2 / span 1 / span 1;
+`;
+const Offer3 = styled(Offer1)`
+  grid-area: 2 / 1 / span 1 / span 1;
+`;
+const Offer4 = styled(Offer1)`
+  grid-area: 2 / 2 / span 1 / span 1;
+`;
 
 class ViewDeals extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       otherSites: [],
-      otherPrices: 'n/a',
     };
   }
 
@@ -65,12 +95,20 @@ class ViewDeals extends React.Component {
     return (
       <div>
         <LowestPrice>Lowest price we found</LowestPrice>
-        <MainOfferSite>Main Offer</MainOfferSite>
+        <MainOffer>
+          <MainOfferSite>WanderNest</MainOfferSite>
+          <MainOfferPrice>{this.props.mainPrice}</MainOfferPrice>
+        </MainOffer>
         <Cancellation>Free cancellation until 6/1/20</Cancellation>
         <ViewDealWrapper>
           <ViewDealButton>View Deal</ViewDealButton>
         </ViewDealWrapper>
-        <OtherOffers>other offers here</OtherOffers>
+        <OtherOffers>
+          <Offer1>first site...................</Offer1>
+          <Offer2>second site..................</Offer2>
+          <Offer3>third site...................</Offer3>
+          <Offer4>fourth site....................</Offer4>
+        </OtherOffers>
       </div>
     );
   }
